e4d8c713f8b46facfd75955ad7f6307cae9bb752
EternityZXC <stafeckis.gatis@gmail.com>
2026-08-02 02:11:07 +0300

    Add new menu option

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from verify_session import check_all_sessions
 MENU = """
 1) Create sessions from tdata folders (tdata_accounts/ -> sessions/)
 2) Check all sessions in sessions/
+3) Send test message to a username (avoid)
 0) Exit
 """
 
@@ -20,6 +21,8 @@ async def run() -> None:
             await create_sessions_from_tdata()
         elif choice == "2":
             await check_all_sessions()
+        elif choice == "3":
+            await check_all_sessions(send_test_message_flag=True, target_username=input("Enter the username to send a test message to (without @): ").strip())
         elif choice == "0":
             break
         else:
